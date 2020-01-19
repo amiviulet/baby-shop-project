4744c4b374b04a1bcc5e8b4f685db020412cf505
State Valentin <statevalentin1998@gmail.com>
2020-01-19 19:08:45 +0000

    fixes

---
+++ script.js
@@ -0,0 +1,32 @@
+var navbarLarge = document.getElementById("navbar-large");
+var navbarMobile = document.getElementById("navbar-mobile");
+var stickyLarge = navbarLarge.offsetTop;
+var stickyMobile = navbarMobile.offsetTop;
+var featuredItem = document.getElementsByClassName("featured-item") ;
+
+addEventListener("mouseover", function(){
+  // this.classList.add("featured-item-hover")
+
+})
+
+
+function becomeStickyLarge() {
+    if (window.pageYOffset > stickyLarge) {
+      navbarLarge.classList.add("sticky-navbar");
+    } else {
+      navbarLarge.classList.remove("sticky-navbar");
+    }
+  }
+
+  function becomeStickyMobile() {
+    if (window.pageYOffset > stickyMobile) {
+      navbarMobile.classList.add("sticky-navbar");
+    } else {
+      navbarMobile.classList.remove("sticky-navbar");
+    }
+  }
+
+  window.onscroll = function() {
+    becomeStickyLarge();
+    becomeStickyMobile();
+  }
