26a72fe8cf45b361246132793d77e7b2e21ca6e4
amiviulet <amiviulet@yahoo.com>
2020-02-07 14:07:19 +0000

    footer and go-to-top button v1

--- script.js
+++ script.js
@@ -46,3 +46,20 @@ function(){
   $(this).removeClass("active-button");
 }
 );
+
+
+var mybutton = document.getElementById("scroll-top-button");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
